44fbbbbe4739ffcedb7410e08bd57059cabb3ce6
SairMezaG <z.meza.g@hotmail.com>
2024-06-17 13:06:52 -0500

    Se intenta corregir un error en los datos, pero sigue el problema

--- BackEndProyecto/appProyecto/urls.py
+++ BackEndProyecto/appProyecto/urls.py
@@ -10,11 +10,13 @@ router.register("api/loteCafe", LoteCafeViewSet, "loteCafe")
 
 router.register("api/tipoProceso", TipoProcesoViewSet, "tipoProceso")
 
-router.register("api/variedad", LoteCafeViewSet, "variedad")
+router.register("api/variedad", VariedadViewSet, "variedad")
 
 router.register("api/maquina", MaquinaViewSet, "maquina")
 
 router.register("api/datos", DatosViewSet, "datos")
+
+router.register("api/seguimiento", SeguimientoViewSet, "seguimiento")
 
 urlpatterns = router.urls
  
